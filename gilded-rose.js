cc673c45ffcd3512c713d2d242b57c7dfe640b04
joelaspe <joe.laspe@gmail.com>
2023-07-06 16:10:35 -0700

    Fixed bug where Conjured item would not decrement by 4 when past sellIn day

--- gilded-rose.js
+++ gilded-rose.js
@@ -71,7 +71,11 @@ export class Conjured extends Item {
     super(name, sellIn, quality);
   }
   decrementQuality() {
-    this.quality -= 2;
+    if(this.sellIn <= 0) {
+      this.quality -= 4;
+    } else {
+      this.quality -= 2;
+    }    
     if(this.quality < 0) {
       this.quality = 0;
     }
